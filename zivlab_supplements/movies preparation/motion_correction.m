3ce34ad73c46b4fb9784277678bac953b036da8e
Linor BT <astrolinor@gmail.com>
2018-02-21 13:39:08 +0200

    Turn the motion correction code into function

--- zivlab_supplements/movies preparation/motion_correction.m
+++ zivlab_supplements/movies preparation/motion_correction.m
@@ -1,14 +1,8 @@
-% This file  preformes concatenatination, motion correction and cropping
+function motion_correction(input_filenames, saving_filename)
+% This function  preformes concatenatination, motion correction and cropping
 % for the neuronal videos trough mosaic. It is suitible for Bambi movies,
 % but trough changing filenames can work with other movies found in the
 % same folder.
-base_dir = 'Z:\experiments\projects\bambi\linear_track_2\data\2_groups\day_1\c51m4\bambi_2017_12_12__09_11_57\';
-filenames = {[base_dir, 'f0_frames.tif'], [base_dir, 'f0_frames.tif']};
-
-save_dir = '';
-save_filename = 'motion_corrected_neuronal.tiff';
-%%
-mosaic.initialize()
 
 %% define motion correction parameters:
 motionCorrectionType = 'Translation'; % 'Translation' 'Rigid','Similarity','Skew'
@@ -18,9 +12,9 @@ applyMeanFilter = true;
 meanSubtraction = true;
 
 %% Load movies
-mosaic_movies = cell(1, length(filenames));
-for i=1:length(filenames)
-    mosaic_movies{i} = mosaic.loadMovieTiff(filenames{i});
+mosaic_movies = cell(1, length(input_filenames));
+for i=1:length(input_filenames)
+    mosaic_movies{i} = mosaic.loadMovieTiff(input_filenames{i});
 end
 
 %% Concatenate movies
@@ -28,16 +22,15 @@ movies_list = mosaic.List('mosaic.Movie', mosaic_movies);
 mosaic_movie = mosaic.concatenateMovies(movies_list, 'gapTime', 0);
 
 %% Choose roi for motion correction
-msgbox('close figure for choosinf ROI')
+msgbox('close figure for choosinf ROI');
 tissueRoi = mosaic.EllipseRoi(mosaic.Point(200, 110), mosaic.Point(50, 70));
-tissueRoi.edit(mosaic_movie); 
+tissueRoi.edit(mosaic_movie);
 
 %% Perform motion correction
-[tifMovie_Registered ,motionParameters] = mosaic.motionCorrectMovie(...
+[tifMovie_Registered ,~] = mosaic.motionCorrectMovie(...
     mosaic_movie, 'parallelProcess',parallel, ...
     'motionType',motionCorrectionType,'invertImage',invIm,'roi',tissueRoi, ...
     'subtractSpatialMean', meanSubtraction,'applySpatialMean', applyMeanFilter);
-estimatedTranslations = motionParameters.getList('types', {'mosaic.Trace'});
 
 %% Crop movie
 tifMovie_Registered.view()
@@ -51,4 +44,5 @@ cropped_movie = mosaic.cropMovie(tifMovie_Registered, position(1),position(2),po
 cropped_movie.view()
 
 %% Save results
-mosaic.saveOneObject(cropped_movie,fullfile(save_dir, save_filename));
+mosaic.saveMovieTiff(cropped_movie,saving_filename);
+end
